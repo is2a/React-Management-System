31b0a36bd0e7e200d98d2dbbc5ef444f8607f95f
Ehrdev <ehrdev@vdi.hyundaicorp.com>
2020-08-06 08:16:46 +0900

    데이터베이스 구축진행

--- client/src/App.js
+++ client/src/App.js
@@ -7,36 +7,51 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
   root : {
     width : "100%",
-    magineTop : theme.spacing.unit * 3,
+    marginTop : theme.spacing(3),
     overflowX : "auto"
   },
   table : {
     minWidth : 1080
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
 });
 
 class App extends Component{
   state = {
-    customers: ''
+    customers: '',
+    completed: 0
   }
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({customers: res}))
       .catch(err => console.log(err));
   }
   
-  callApi = async() => {
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  callApi = async () => {
     const response = await fetch('/api/customers');
     const body = await response.json();
     return body;
   }
-  
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1});
+  }
+
   render(){
     const{ classes } = this.props; //해당 컴포넌트가 호출된 곳에서 정의한 props를 포함
     return(
@@ -53,9 +68,16 @@ class App extends Component{
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c => {
+            {this.state.customers ?
+              this.state.customers.map(c => {
               return <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
-            })}
+            }) : 
+            <TableRow>
+              <TableCell colSpan="6" align="center">
+                <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+              </TableCell>
+            </TableRow>
+            }
           </TableBody>
         </Table>
       </Paper>
